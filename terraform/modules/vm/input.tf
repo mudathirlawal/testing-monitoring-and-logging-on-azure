2fc78c9bf348e07cb1db51aaf6bf86378baedd9a
Mudathir Lawal <nigercode@gmail.com>
2021-04-29 12:35:21 +0100

    feat: Add vm configuration

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,19 @@
 
+# Azure GUIDS
+variable "client_id" {}
+variable "tenant_id" {}
+variable "client_secret" {}
+variable "vm_admin_username" {}
+
+# Resource Group/Location
+variable "location" {}
+variable "resource_type" {}
+variable "resource_group" {}
+variable "application_type" {}
+
+# Network
+variable "subnet_id" {}
+variable "address_space" {}
+variable "address_prefix_test" {}
+variable "public_ip_address_id" {}
+variable "virtual_network_name" {}
