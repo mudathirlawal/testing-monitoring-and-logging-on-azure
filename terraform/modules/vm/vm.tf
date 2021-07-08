e0010e5c8c05781c5a73c7182f857472821152f8
Mudathir Lawal <nigercode@gmail.com>
2021-07-09 00:35:17 +0100

    feat: Use the provider given in main.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,25 +1,25 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.66.0"
-    }
-  }
-}
-terraform {  
-  backend "azurerm" {
-    # storage_account_name = "tstate1096"
-    # container_name       = "tstate"
-    # key                  = "terraform.tfstate"
-  }
-}
-provider "azurerm" {
-  # tenant_id       = var.tenant_id
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  features {}
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=2.66.0"
+#     }
+#   }
+# }
+# terraform {  
+#   backend "azurerm" {
+#     # storage_account_name = "tstate1096"
+#     # container_name       = "tstate"
+#     # key                  = "terraform.tfstate"
+#   }
+# }
+# provider "azurerm" {
+#   # tenant_id       = var.tenant_id
+#   # subscription_id = var.subscription_id
+#   # client_id       = var.client_id
+#   # client_secret   = var.client_secret
+#   features {}
+# }
 resource "azurerm_network_interface" "test" {
   name                = "${var.application_type}-${var.resource_type}-nic"
   location            = var.location
