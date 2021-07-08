7a7ff9e3c6765f3f3d9462e4546cb491f3ceb142
Mudathir Lawal <nigercode@gmail.com>
2021-07-08 11:50:04 +0100

    build: Remove reference to backend

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -14,10 +14,10 @@ terraform {
   }
 }
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
   features {}
 }
 module "resource_group" {
