8c4d9519ed8cff99f5c8013324647f4844cce117
Mudathir Lawal <nigercode@gmail.com>
2021-07-10 23:08:59 +0100

    feat: Add correct ssh key path

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -44,8 +44,8 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = var.vm_admin_username
-    # public_key = file("~/.ssh/id_rsa.pub")
-    public_key = file("/home/vsts/work/_temp/id_rsa.pub")
+    public_key = file("~/.ssh/id_rsa.pub")
+    # public_key = file("/home/vsts/_work/_temp/id_rsa.pub")
   }
   
   os_disk {
