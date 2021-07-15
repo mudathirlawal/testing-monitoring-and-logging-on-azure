c91ed9f28402360988a5673102d33e82f612438a
Mudathir Lawal <nigercode@gmail.com>
2021-07-15 02:59:45 +0100

    feat: Remove scafolding from source

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -29,8 +29,6 @@ resource "azurerm_network_interface" "test" {
     name                          = "internal"
     subnet_id                     = "var.subnet_id"
     public_ip_address_id          = "var.public_ip_address_id"
-    # subnet_id = var.subnet_id
-    # public_ip_address_id = var.public_ip_address_id
     private_ip_address_allocation = "Dynamic"
   }
 }
