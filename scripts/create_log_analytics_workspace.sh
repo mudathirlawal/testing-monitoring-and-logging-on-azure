4d7c13effc420cfd0f3a4de8013b08f7f82fd90e
Mudathir Lawal <nigercode@gmail.com>
2021-07-21 07:29:06 +0100

    feat: update workspace create script

--- scripts/create_log_analytics_workspace.sh
+++ scripts/create_log_analytics_workspace.sh
@@ -3,6 +3,11 @@
 # Use the following microsoft walkthrough: 
 # https://docs.microsoft.com/en-us/azure/azure-monitor/logs/quick-create-workspace-cli
 
-az deployment group create --resource-group <my-resource-group> \
-    --name <my-deployment-name> \
-    --template-file deploylaworkspacetemplate.json
+# GUIDE:
+# az deployment group create --resource-group <my-resource-group> \
+#     --name <my-deployment-name> \
+#     --template-file deploylaworkspacetemplate.json
+
+az deployment group create --resource-group qr-analytics-rg \
+	--name QRWorkspaceTemplate \
+	--template-file deployqrworkspacetemplate.json
