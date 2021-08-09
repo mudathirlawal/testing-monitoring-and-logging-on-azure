e35bbd3b46e0273d42874dae97738e7f971e0f87
Mudathir Lawal <nigercode@gmail.com>
2021-08-09 14:31:54 +0100

    test: Modify selenium functional test

--- automated-testing/selenium/run_ui_tests.py
+++ automated-testing/selenium/run_ui_tests.py
@@ -20,7 +20,8 @@ def run_ui_tests(user, password):
     driver.get('https://www.google.nl/')
 
     # driver = webdriver.Chrome()
-    print(log_timestamp() + 'Browser started successfully. Navigating to the login page ...')
+    print(log_timestamp() + 'Browser started successfully.')
+    print(log_timestamp() + 'Navigating to the login page.')
     driver.get('https://www.saucedemo.com/')
 
     print(log_timestamp() + 'Loging in to https://www.saucedemo.com/')
@@ -30,29 +31,29 @@ def run_ui_tests(user, password):
         'input[data-test="password"]').send_keys(password)
     driver.find_element_by_css_selector('input[value=Login]').click()
 
-    print(log_timestamp() + 'Searching for Products ...')
+    print(log_timestamp() + 'Searching for Products.')
     headerLabel = driver.find_element_by_class_name('header_secondary_container').text
     assert "PRODUCTS" in headerLabel
     print(log_timestamp() + 'Successfully logged in ' + user + '.')
 
-    print(log_timestamp() + 'Selecting products ...')
+    print(log_timestamp() + 'Selecting products.')
     products = driver.find_elements_by_css_selector('.inventory_item')
 
-    print(log_timestamp() + 'Adding products to cart ...')
+    print(log_timestamp() + 'Adding products to cart.')
     for product in products:
         product_name = product.find_element_by_css_selector(
             '.inventory_item_name').text
         product.find_element_by_css_selector('button.btn_inventory').click()
         print(log_timestamp() + product_name + ' successfully added to cart.')
 
-    print(log_timestamp() + 'Verifying if cart has been populated with 6 products ...')
+    print(log_timestamp() + 'Verifying if cart has been populated with 6 products.')
     cart_label = driver.find_element_by_css_selector(
         '.shopping_cart_badge').text
     assert cart_label == '6'
 
     print(log_timestamp() + 'Navigating to shopping cart ...')
     driver.find_element_by_css_selector('a.shopping_cart_link').click()
-    assert '/cart.html' in driver.current_url, log_timestamp() + 'Navigation to shopping cart unsuccessful'
+    assert '/cart.html' in driver.current_url, log_timestamp() + 'Navigation to shopping cart unsuccessful.'
 
     print(log_timestamp() + 'Removing products from cart.')
     cart_products = driver.find_elements_by_css_selector('.cart_item')
@@ -62,7 +63,7 @@ def run_ui_tests(user, password):
         product.find_element_by_css_selector('button.cart_button').click()
         print(log_timestamp() + product_name + ' successfully removed from cart.')
 
-    print(log_timestamp() + 'Confirming that shopping cart is empty ...')
+    print(log_timestamp() + 'Confirming that shopping cart is empty.')
 
     if driver.find_elements_by_css_selector('.shopping_cart_badge'):
         cart_emptiness_flag = False
